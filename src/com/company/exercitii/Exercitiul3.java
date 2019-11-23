796cffdc977fe1836c0854bd9be097da6504358c
Birau Daniela <biraudaniela@yahoo.com>
2019-11-23 13:13:00 +0200

    Solved problem 3

---
+++ src/com/company/exercitii/Exercitiul3.java
@@ -0,0 +1,27 @@
+package com.company.exercitii;
+//Create an array and initialize it with the first 100 numbers that can be divided by 11.
+// Create one method that initializes the array and one method that prints the contents of the array.
+// Call them in main.
+
+public class Exercitiul3 {
+    public static void main(String[] args) {
+        int[] array;
+        array = initArray(100);
+        printArray(array); }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    private static int[] initArray(int limit) {
+        int[] array = new int[limit];
+        int count = 0;
+        for (int i = 0; count < limit; i++) {
+            if (i % 11 == 0) {
+                array[count++] = i;
+            }
+        }return array;
+    }
+}
